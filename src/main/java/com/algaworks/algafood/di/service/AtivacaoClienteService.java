c4430fd6ec2531c4a58062714e7ecc16797075e3
paulinhagrio <paulinhagrio@hotmail.com>
2023-07-19 20:20:24 -0300

    2.21_Criando métodos de callback do clico de vida dos beans

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -2,6 +2,9 @@ package com.algaworks.algafood.di.service;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
@@ -12,12 +15,22 @@ import com.algaworks.algafood.di.notificacao.Notificador;
 
 
 
-@Component
+//@Component
 public class AtivacaoClienteService {
 	
 	@Qualifier("email")
 	@Autowired
 	private Notificador notificador;	
+	
+	//@PostConstruct
+	public void init() {
+		System.out.println("INIT" + notificador);
+	}
+	
+	//@PreDestroy
+	public void destroy() {
+		System.out.println("DESTROY");
+	}
 		
 		
 	public void ativar(Cliente cliente) {		
